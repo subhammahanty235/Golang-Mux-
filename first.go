2d06df0f57f6d877e46b2a887f77bb8c195aee54
subhammahanty235 <subhammahanty235@gmail.com>
2023-02-07 23:47:20 +0530

    for loop

--- first.go
+++ first.go
@@ -15,4 +15,10 @@ func main() {
 	// a := 4
 	var arr = [...]int{2, 3, 5, 6, 7, 8, 5, 4, 6, 8, 5, 78, 5, 8, 6}
 	fmt.Print(arr[0:4])
+	// loops
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
 }
+
+//
