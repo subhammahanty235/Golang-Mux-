c9bc361d5fedcb3edae88cf983d4ce3c544a97d3
subhammahanty235 <subhammahanty235@gmail.com>
2023-02-07 23:40:28 +0530

    variables and array

--- first.go
+++ first.go
@@ -1,7 +1,18 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// fmt.Println("Hello World")
+// }
+
+// Declaring variables
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// a := 4
+	var arr = [...]int{2, 3, 5, 6, 7, 8, 5, 4, 6, 8, 5, 78, 5, 8, 6}
+	fmt.Print(arr[0:4])
 }
